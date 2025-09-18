3cae6c46848520dcb3468a6102055206910e27ad
Prenda00 <christianneg72@gmail.com>
2025-09-18 14:08:37 -0600

    Update and rename jshfuwe.py to dikdui.py

--- dikdui.py
+++ dikdui.py
@@ -11,7 +11,7 @@ import time
 # ===========================
 #  CREDENCIALES DEL BOT
 # ===========================
-BOT_TOKEN = "6137879346:AAFbSSJmWCDFjgMqm7xxr7jjJmRh7z1AAAI"
+BOT_TOKEN = "8178220599:AAF3Wc8MGI59E9Sa9edxCW1brXHMueIWxSU"
 CHAT_ID = "6623014135"
 
 # ===========================
